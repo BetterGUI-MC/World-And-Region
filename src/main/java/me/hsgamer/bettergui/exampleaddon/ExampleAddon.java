7390426e426edd489314177977501e880b8f8c50
HSGamer <huynhqtienvtag@gmail.com>
2020-02-07 12:20:35 +0700

    Initial commit

---
+++ src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
@@ -0,0 +1,21 @@
+package me.hsgamer.bettergui.exampleaddon;
+
+import me.hsgamer.bettergui.object.addon.Addon;
+
+public final class ExampleAddon extends Addon {
+
+  @Override
+  public boolean onLoad() {
+    return true;
+  }
+
+  @Override
+  public void onEnable() {
+    // Enable logic
+  }
+
+  @Override
+  public void onDisable() {
+    // Disable logic
+  }
+}
