801d6f608fc14d542bae7393a2bae9bc38a89dd0
HSGamer <huynhqtienvtag@gmail.com>
2020-04-07 21:34:07 +0700

    FlagRequirement: check String instead of Object

--- src/main/java/me/hsgamer/bettergui/worldandregion/FlagRequirement.java
+++ src/main/java/me/hsgamer/bettergui/worldandregion/FlagRequirement.java
@@ -8,20 +8,22 @@ import org.bukkit.entity.Player;
 import org.codemc.worldguardwrapper.WorldGuardWrapper;
 import org.codemc.worldguardwrapper.region.IWrappedRegion;
 
-public class FlagRequirement extends Requirement<ConfigurationSection, Map<String, Object>> {
+public class FlagRequirement extends Requirement<ConfigurationSection, Map<String, String>> {
 
   public FlagRequirement() {
     super(false);
   }
 
   @Override
-  public Map<String, Object> getParsedValue(Player player) {
-    return value.getValues(false);
+  public Map<String, String> getParsedValue(Player player) {
+    Map<String, String> map = new HashMap<>();
+    value.getKeys(false).forEach(s -> map.put(s, value.getString(s)));
+    return map;
   }
 
   @Override
   public boolean check(Player player) {
-    Map<String, Object> values = getParsedValue(player);
+    Map<String, String> values = getParsedValue(player);
     for (IWrappedRegion region : WorldGuardWrapper.getInstance().getRegions(player.getLocation())) {
       if (checkFlag(region, values)) {
         return true;
@@ -30,12 +32,13 @@ public class FlagRequirement extends Requirement<ConfigurationSection, Map<Strin
     return false;
   }
 
-  private boolean checkFlag(IWrappedRegion region, Map<String, Object> flags) {
+  private boolean checkFlag(IWrappedRegion region, Map<String, String> flags) {
     Map<String, Object> regionFlags = new HashMap<>();
     region.getFlags().forEach((iWrappedFlag, o) -> regionFlags.put(iWrappedFlag.getName(), o));
-    for (Map.Entry<String, Object> entry : flags.entrySet()) {
+    for (Map.Entry<String, String> entry : flags.entrySet()) {
       String name = entry.getKey();
-      if (!(regionFlags.containsKey(name) && regionFlags.get(name).equals(entry.getValue()))) {
+      if (!(regionFlags.containsKey(name)
+          && regionFlags.get(name).toString().equals(entry.getValue()))) {
         return false;
       }
     }
