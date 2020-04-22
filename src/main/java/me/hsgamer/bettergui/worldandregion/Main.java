a8fd83c090805809ce6824643078872e686d5d22
HSGamer <huynhqtienvtag@gmail.com>
2020-04-22 19:54:14 +0700

    add {region}

--- src/main/java/me/hsgamer/bettergui/worldandregion/Main.java
+++ src/main/java/me/hsgamer/bettergui/worldandregion/Main.java
@@ -1,7 +1,11 @@
 package me.hsgamer.bettergui.worldandregion;
 
+import java.util.Set;
 import me.hsgamer.bettergui.builder.RequirementBuilder;
+import me.hsgamer.bettergui.manager.VariableManager;
 import me.hsgamer.bettergui.object.addon.Addon;
+import org.codemc.worldguardwrapper.WorldGuardWrapper;
+import org.codemc.worldguardwrapper.region.IWrappedRegion;
 
 public final class Main extends Addon {
 
@@ -17,6 +21,13 @@ public final class Main extends Addon {
       getPlugin().getLogger().info("Added WorldGuard support");
       RequirementBuilder.register("region", RegionRequirement.class);
       RequirementBuilder.register("flag", FlagRequirement.class);
+      VariableManager.register("region", ((player, s) -> {
+        Set<IWrappedRegion> regionSet = WorldGuardWrapper.getInstance().getRegions(player.getLocation());
+        if (!regionSet.isEmpty()) {
+          return regionSet.iterator().next().getId();
+        }
+        return "";
+      }));
     }
   }
 }
