0169dbb8d5ce72e5cc2fda5ccad87830f671b6f7
HSGamer <huynhqtienvtag@gmail.com>
2020-04-23 17:55:29 +0700

    FlagRequirement: use queryFlag

--- src/main/java/me/hsgamer/bettergui/worldandregion/FlagRequirement.java
+++ src/main/java/me/hsgamer/bettergui/worldandregion/FlagRequirement.java
@@ -2,43 +2,35 @@ package me.hsgamer.bettergui.worldandregion;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import me.hsgamer.bettergui.object.Requirement;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.codemc.worldguardwrapper.WorldGuardWrapper;
-import org.codemc.worldguardwrapper.region.IWrappedRegion;
+import org.codemc.worldguardwrapper.flag.IWrappedFlag;
 
-public class FlagRequirement extends Requirement<ConfigurationSection, Map<String, String>> {
+public class FlagRequirement extends
+    Requirement<ConfigurationSection, Map<IWrappedFlag<?>, Object>> {
 
   public FlagRequirement() {
     super(false);
   }
 
   @Override
-  public Map<String, String> getParsedValue(Player player) {
-    Map<String, String> map = new HashMap<>();
-    value.getKeys(false).forEach(s -> map.put(s, value.getString(s)));
+  public Map<IWrappedFlag<?>, Object> getParsedValue(Player player) {
+    Map<IWrappedFlag<?>, Object> map = new HashMap<>();
+    value.getValues(false).forEach(
+        (s, o) -> WorldGuardWrapper.getInstance().getFlag(s, o.getClass())
+            .ifPresent(iWrappedFlag -> map.put(iWrappedFlag, o)));
     return map;
   }
 
   @Override
   public boolean check(Player player) {
-    Map<String, String> values = getParsedValue(player);
-    for (IWrappedRegion region : WorldGuardWrapper.getInstance().getRegions(player.getLocation())) {
-      if (checkFlag(region, values)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private boolean checkFlag(IWrappedRegion region, Map<String, String> flags) {
-    Map<String, Object> regionFlags = new HashMap<>();
-    region.getFlags().forEach((iWrappedFlag, o) -> regionFlags.put(iWrappedFlag.getName(), o));
-    for (Map.Entry<String, String> entry : flags.entrySet()) {
-      String name = entry.getKey();
-      if (!(regionFlags.containsKey(name)
-          && regionFlags.get(name).toString().equals(entry.getValue()))) {
+    for (Map.Entry<IWrappedFlag<?>, Object> entry : getParsedValue(player).entrySet()) {
+      Optional<?> optional = WorldGuardWrapper.getInstance()
+          .queryFlag(player, player.getLocation(), entry.getKey());
+      if (!optional.isPresent() || !optional.get().equals(entry.getValue())) {
         return false;
       }
     }
