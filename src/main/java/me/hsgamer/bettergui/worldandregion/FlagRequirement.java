e7f562806025752d728f87a49a119d9325345ac1
HSGamer <huynhqtienvtag@gmail.com>
2020-04-23 19:18:12 +0700

    compare as String

--- src/main/java/me/hsgamer/bettergui/worldandregion/FlagRequirement.java
+++ src/main/java/me/hsgamer/bettergui/worldandregion/FlagRequirement.java
@@ -10,27 +10,28 @@ import org.codemc.worldguardwrapper.WorldGuardWrapper;
 import org.codemc.worldguardwrapper.flag.IWrappedFlag;
 
 public class FlagRequirement extends
-    Requirement<ConfigurationSection, Map<IWrappedFlag<?>, Object>> {
+    Requirement<ConfigurationSection, Map<IWrappedFlag<?>, String>> {
 
   public FlagRequirement() {
     super(false);
   }
 
   @Override
-  public Map<IWrappedFlag<?>, Object> getParsedValue(Player player) {
-    Map<IWrappedFlag<?>, Object> map = new HashMap<>();
+  public Map<IWrappedFlag<?>, String> getParsedValue(Player player) {
+    Map<IWrappedFlag<?>, String> map = new HashMap<>();
     value.getValues(false).forEach(
-        (s, o) -> WorldGuardWrapper.getInstance().getFlag(s, o.getClass())
-            .ifPresent(iWrappedFlag -> map.put(iWrappedFlag, o)));
+        (s, o) -> WorldGuardWrapper.getInstance().getFlag(s, String.class)
+            .ifPresent(
+                iWrappedFlag -> map.put(iWrappedFlag, parseFromString(String.valueOf(o), player))));
     return map;
   }
 
   @Override
   public boolean check(Player player) {
-    for (Map.Entry<IWrappedFlag<?>, Object> entry : getParsedValue(player).entrySet()) {
+    for (Map.Entry<IWrappedFlag<?>, String> entry : getParsedValue(player).entrySet()) {
       Optional<?> optional = WorldGuardWrapper.getInstance()
           .queryFlag(player, player.getLocation(), entry.getKey());
-      if (!optional.isPresent() || !optional.get().equals(entry.getValue())) {
+      if (!optional.isPresent() || !String.valueOf(optional.get()).equals(entry.getValue())) {
         return false;
       }
     }
