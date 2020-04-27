e4b9ff153f1f5acb27b1cb49a43e48f66f799063
HSGamer <huynhqtienvtag@gmail.com>
2020-04-27 07:58:45 +0700

    add __global__ if present

--- src/main/java/me/hsgamer/bettergui/worldandregion/Utils.java
+++ src/main/java/me/hsgamer/bettergui/worldandregion/Utils.java
@@ -1,10 +1,11 @@
 package me.hsgamer.bettergui.worldandregion;
 
+import static org.codemc.worldguardwrapper.WorldGuardWrapper.getInstance;
+
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.Set;
 import org.bukkit.Location;
-import org.codemc.worldguardwrapper.WorldGuardWrapper;
 import org.codemc.worldguardwrapper.region.IWrappedRegion;
 
 public final class Utils {
@@ -13,12 +14,11 @@ public final class Utils {
   }
 
   public static Optional<IWrappedRegion> getMaxPriorityRegion(Location location) {
-    Set<IWrappedRegion> regionSet = WorldGuardWrapper.getInstance()
-        .getRegions(location);
+    Set<IWrappedRegion> regionSet = getInstance().getRegions(location);
+    getInstance().getRegion(location.getWorld(), "__global__").ifPresent(regionSet::add);
     if (!regionSet.isEmpty()) {
       return regionSet.stream().max(Comparator.comparingInt(IWrappedRegion::getPriority));
-    } else {
-      return WorldGuardWrapper.getInstance().getRegion(location.getWorld(), "__global__");
     }
+    return Optional.empty();
   }
 }
