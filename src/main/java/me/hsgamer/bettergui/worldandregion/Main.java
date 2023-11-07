3b498974155369653b92f435bf6c773739f64412
hsgamer <huynhqtienvtag@gmail.com>
2023-11-08 00:05:35 +0700

    update parent

--- src/main/java/me/hsgamer/bettergui/worldandregion/Main.java
+++ src/main/java/me/hsgamer/bettergui/worldandregion/Main.java
@@ -1,14 +1,17 @@
 package me.hsgamer.bettergui.worldandregion;
 
+import me.hsgamer.bettergui.api.addon.GetPlugin;
 import me.hsgamer.bettergui.builder.RequirementBuilder;
-import me.hsgamer.hscore.bukkit.addon.PluginAddon;
-import me.hsgamer.hscore.variable.VariableManager;
+import me.hsgamer.hscore.common.StringReplacer;
+import me.hsgamer.hscore.expansion.common.Expansion;
+import me.hsgamer.hscore.variable.VariableBundle;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
-public final class Main extends PluginAddon {
+public final class Main implements Expansion, GetPlugin {
+    private final VariableBundle variableBundle = new VariableBundle();
 
     @Override
     public void onEnable() {
@@ -20,7 +23,7 @@ public final class Main extends PluginAddon {
             RequirementBuilder.INSTANCE.register(FlagRequirement::new, "flag");
             RequirementBuilder.INSTANCE.register(RegionOwnerRequirement::new, "region-owner");
             RequirementBuilder.INSTANCE.register(RegionUserRequirement::new, "region-user");
-            VariableManager.register("region", (original, uuid) -> {
+            variableBundle.register("region", StringReplacer.of((original, uuid) -> {
                 Player player = Bukkit.getPlayer(uuid);
                 if (player == null) {
                     return "";
@@ -30,14 +33,19 @@ public final class Main extends PluginAddon {
                     return list.get(0);
                 }
                 return "";
-            });
-            VariableManager.register("flag_", (original, uuid) -> {
+            }));
+            variableBundle.register("flag_", StringReplacer.of((original, uuid) -> {
                 Player player = Bukkit.getPlayer(uuid);
                 if (player == null) {
                     return "";
                 }
                 return WorldGuardUtil.queryFlag(player, player.getLocation(), original);
-            });
+            }));
         }
+    }
+
+    @Override
+    public void onDisable() {
+        variableBundle.unregisterAll();
     }
 }
